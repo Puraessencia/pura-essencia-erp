21d3aebf52246c1ff848eee86ddb05f08d07b313
Pura Essência <dvnosecreto@gmail.com>
2026-07-25 04:35:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const dashboard = {
+
     produtos:56,
     pecas:734,
     estoqueBaixo:12,
-    valor:14580
+    valor:"R$ 14.580,00"
+
 };
 
-document
-.getElementById("totalProdutos")
-.innerText = dashboard.produtos;
+document.getElementById("produtos").innerText =
+dashboard.produtos;
+
+document.getElementById("pecas").innerText =
+dashboard.pecas;
+
+document.getElementById("baixo").innerText =
+dashboard.estoqueBaixo;
+
+document.getElementById("valor").innerText =
+dashboard.valor;
